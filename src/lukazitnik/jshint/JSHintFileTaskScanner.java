ff41948c80035520d123abd5bcb6e1b23dca0a3f
Luka Zitnik <luka.zitnik@gmail.com>
2015-03-09 01:27:03 +0100

    complete dependencies for the first build&install

--- src/lukazitnik/jshint/JSHintFileTaskScanner.java
+++ src/lukazitnik/jshint/JSHintFileTaskScanner.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.netbeans.spi.tasklist.FileTaskScanner;
 import org.netbeans.spi.tasklist.Task;
 import org.openide.filesystems.FileObject;
+import org.openide.util.NbBundle;
 
 /**
  *
@@ -12,21 +13,24 @@ import org.openide.filesystems.FileObject;
  */
 public class JSHintFileTaskScanner extends FileTaskScanner {
 
-    private LinkedList<Task> tasks = new LinkedList<Task>();
-
-    public JSHintFileTaskScanner(String displayName, String description, String optionsPath) {
-        super(displayName, description, optionsPath);
+    public JSHintFileTaskScanner(String displayName, String description) {
+        super(displayName, description, null);
     }
 
     public static JSHintFileTaskScanner create() {
-        return new JSHintFileTaskScanner("JSHint", "JSHint", null);
+        return new JSHintFileTaskScanner(
+                NbBundle.getMessage(JSHintFileTaskScanner.class, "LBL_task"),
+                NbBundle.getMessage(JSHintFileTaskScanner.class, "DESC_task")
+        );
     }
 
     @Override
     public List<? extends Task> scan(FileObject fo) {
 
+        LinkedList<Task> tasks = new LinkedList<Task>();
+
         if (!fo.isFolder()) {
-            tasks.add(Task.create(fo, "", fo.getNameExt(), 1));
+            tasks.add(Task.create(fo, "nb-tasklist-jshint", fo.getNameExt(), 1));
         }
 
         return tasks;
