356ff746f22a205e127d198a9ddca05ffa9bc018
Luka Zitnik <luka.zitnik@gmail.com>
2015-07-03 12:22:56 +0200

    show progress indicator

--- src/lukazitnik/jshint/Installer.java
+++ src/lukazitnik/jshint/Installer.java
@@ -11,6 +11,8 @@ import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.JTextComponent;
 import org.netbeans.api.editor.EditorRegistry;
+import org.netbeans.api.progress.ProgressHandle;
+import org.netbeans.api.progress.ProgressHandleFactory;
 import org.netbeans.editor.AnnotationDesc;
 import org.netbeans.editor.Annotations;
 import org.netbeans.editor.Utilities;
@@ -46,8 +48,14 @@ public class Installer extends ModuleInstall {
 
                         @Override
                         public void run() {
+                            String fileName = NbEditorUtilities.getFileObject(d).getNameExt();
+                            ProgressHandle progressHandle = ProgressHandleFactory.createHandle("Linting " + fileName);
+                            progressHandle.start();
+
                             JSHint jshint = JSHint.instance;
                             final List<JSHintError> errors = jshint.lint(d);
+
+                            progressHandle.finish();
 
                             SwingUtilities.invokeLater(new Runnable() {
                                 public void run() {
